c7155639527c63e347390e525035d233c940d3e0
NEK0128 <kenrxj@gmail.com>
2014-06-09 20:51:09 +0900

    TopView

---
+++ Jinrou/CLTUserViewController.h
@@ -0,0 +1,13 @@
+//
+//  CLTUserViewController.h
+//  Jinrou
+//
+//  Created by KenjiMatsuda on 2014/06/09.
+//  Copyright (c) 2014年 KenjiMatsuda. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CLTUserViewController : NSObject
+
+@end
